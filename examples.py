f1e92ac1cc19f168731c3a0f229e83dd222e52b7
Alexander Zhilyaev <azh@hellofresh.com>
2016-08-03 09:56:07 +0200

    New users delete endpoint

--- examples.py
+++ examples.py
@@ -2,6 +2,7 @@ from appboy.client import AppboyClient
 
 client = AppboyClient(app_group_id='YOUR_GROUP_ID')
 
+# For create - update users
 r = client.user_track(
     attributes=[{
         'external_id': '1',
@@ -13,6 +14,19 @@ r = client.user_track(
     }],
     events=None,  # if we don't want to send events we set it to None
     purchases=None,
+)
+if r['success']:
+    # do our magic here
+    print 'Success!'
+    print r
+else:
+    print r['client_error']
+    print r['errors']
+
+# For delete users by external_id or appboy_id
+r = client.user_delete(
+    external_ids=['1'],
+    appboy_ids=None,
 )
 if r['success']:
     # do our magic here
