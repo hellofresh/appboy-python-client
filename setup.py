70653a4c7c4979ad75b623d2a57c3d88dff0e793
Sam Park <spark@goodrx.com>
2019-01-17 12:27:08 -0800

    Up minimum tenacity version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 NAME = "braze-client"
 VERSION = "2.1.0"
 
-REQUIRES = ["requests >=2.21.0, <3.0.0", "tenacity >=4.8.0, <6.0.0"]
+REQUIRES = ["requests >=2.21.0, <3.0.0", "tenacity >=5.0.0, <6.0.0"]
 
 EXTRAS = {"dev": ["tox"]}
 
