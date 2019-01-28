a08198f52de2820d1a8445e6d2680614c14c2f95
Pranjal Mittal <mittal.pranjal@gmail.com>
2019-01-28 15:35:10 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 NAME = "braze-client"
-VERSION = "2.1.0"
+VERSION = "2.2.0"
 
 REQUIRES = ["requests >=2.21.0, <3.0.0", "tenacity >=5.0.0, <6.0.0"]
 
